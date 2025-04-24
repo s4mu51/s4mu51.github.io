8886d4993a16b41087c7e9d20824616fb6433674
s4mu51 <s4mu51@gmail.com>
2025-04-24 15:16:10 +0200

    Eliminado _site del control de versiones

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "First Post",
-        "excerpt":"First Post  ","categories": ["Inicial"],
-        "tags": ["HTB","THM","Network"],
-        "url": "/inicial/first-porst/",
-        "teaser": null
-      }]
